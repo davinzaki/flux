5a9c56baae60a14630341946369ca5705862ae4b
Davin Zaki Pradipa <davinzaki12@gmail.com>
2025-10-14 16:05:26 +0700

    feat: add jwt interceptor for refresh token

--- client/src/components/layouts/AuthLayout.tsx
+++ client/src/components/layouts/AuthLayout.tsx
@@ -4,7 +4,7 @@ const AuthLayout = () => {
     return (
         <div className="w-full flex flex-col h-screen sm:flex-row sm:justify-between sm:items-center">
             <div className=" flex flex-col gap-4 p-4">
-                <h1 className="font-intergralcf text-4xl font-bold sm:text-6xl">Welcome to <span className="text-5xl sm:text-6xl sm:underline">Flux</span></h1>
+                <h1 className="font-intergralcf text-4xl font-bold sm:text-6xl">Welcome to <span className="text-5xl sm:text-6xl">Flux</span></h1>
                 <p className="text-lg">An E-Commerce App you only needed.</p>
             </div>
             <Outlet />
--- client/src/services/axios.ts
+++ client/src/services/axios.ts
@@ -4,3 +4,49 @@ export const api = axios.create({
   baseURL: import.meta.env.VITE_APP_URL || "http://localhost:5000/api",
   withCredentials: true,
 });
+
+api.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem("token");
+    if (token) config.headers.Authorization = `Bearer ${token}`;
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+api.interceptors.response.use(
+  (response) => {
+    return response;
+  },
+  async (error) => {
+    const originalRequest = error.config;
+
+    // Check if the error is due to an expired token
+    if (error.response?.status === 401 && !originalRequest._retry) {
+      originalRequest._retry = true; // Avoid infinite loops
+      try {
+        const refreshToken = localStorage.getItem("refreshToken"); // Get the refresh token
+        const response = await axios.post(
+          "http://localhost:5000/api/auth/refresh-token",
+          {
+            token: refreshToken,
+          }
+        );
+
+        const newAccessToken = response.data.accessToken;
+        localStorage.setItem("accessToken", newAccessToken); // Save new token
+        originalRequest.headers["Authorization"] = `Bearer ${newAccessToken}`; // Update request headers
+
+        return api(originalRequest); // Retry the original request
+      } catch (refreshError) {
+        console.error("Token refresh failed:", refreshError);
+        localStorage.clear(); // Clear tokens from storage
+        window.location.href = "/login"; // Redirect to login
+      }
+    }
+
+    return Promise.reject(error); // Forward other errors
+  }
+);
